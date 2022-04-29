c036193563880d30f01b2108888b74c386bd0eaf
mylhb9 <mylhb9@naver.com>
2022-04-30 00:47:34 +0900

    스프링 핵심 원리 이해2 - 객체 지향 원리 적용 / 스프링으로 전환하기

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -2,13 +2,18 @@ package hello.core;
 
 import hello.core.member.*;
 import hello.core.order.Order;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-//        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+        // 스프링 컨테이너라고 보면 된다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
