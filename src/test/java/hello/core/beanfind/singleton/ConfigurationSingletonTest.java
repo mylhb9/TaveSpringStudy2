9f2c729c5c394482e1ff303cf9c99e576f047352
mylhb9 <mylhb9@naver.com>
2022-04-30 19:44:29 +0900

    싱글톤 컨테이너 - 필터

--- src/test/java/hello/core/beanfind/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/beanfind/singleton/ConfigurationSingletonTest.java
@@ -1,4 +1,4 @@
-package hello.core.singleton;
+package hello.core.beanfind.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberRepository;
--- src/test/java/hello/core/beanfind/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/beanfind/singleton/StatefulServiceTest.java
@@ -1,14 +1,11 @@
-package hello.core.singleton;
+package hello.core.beanfind.singleton;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class StatefulServiceTest {
 
